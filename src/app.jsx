49a26c01811c0bfb4de4ecf6c67f4d54fc29341f
ClarXo <imp_rove@naver.com>
2021-02-01 07:32:29 +0900

    Self : get lists of most popular videos

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,28 @@
-import './app.css';
+import "./app.css";
 
-function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  request() {
+    let requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA1kIMYTD7NZMdHfa3nO3QXf4UOBvDtoGw",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div onLoad={this.request}></div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
