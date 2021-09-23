d8db2938bf926436e8c6cc9a8e53e8a5b531f863
ClarXo <imp_rove@naver.com>
2021-09-24 08:48:04 +0900

    reqSearchResult

--- src/app.jsx
+++ src/app.jsx
@@ -6,23 +6,21 @@ class App extends Component {
   key1 = "AIzaSyAAGc5-7w931KT4BPwH9Mv8X9ctk0f1TVc";
   key2 = "AIzaSyBVf5GoN4nFYuoumZXHR6ZeUZvyYOMWgsI";
   state = {
-    searchParam: undefined,
     watching: undefined,
     items: undefined,
   };
   baseURL = "https://youtube.googleapis.com/youtube/v3";
   reqSearch = async (searchParam) => {
-    this.setState({ ...this.setState, searchParam });
     try {
       const response = await fetch(
         `${this.baseURL}/search?part=snippet&maxResults=25&q=${searchParam}&key=${this.key2}`,
         { method: "get" }
       );
       const list = await response.json();
-      const item = list.item;
-      this.setState({ ...this.state, searchParam, item });
+      console.log(list);
+      this.setState({ ...this.state, items: list.items });
     } catch (error) {
-      return console.log("error", error);
+      console.log("error", error);
     }
   };
   reqMost = async () => {
@@ -32,10 +30,14 @@ class App extends Component {
     );
     const result = await response.json();
     const items = result.items;
-    this.setState({ ...this.state, searchParam: undefined, items });
+    this.setState({ ...this.state, items });
   };
   setPlayer = (item) => {
-    this.setState({ ...this.state, watching: item });
+    const watching = this.state.watching;
+    watching ?? this.setState({ ...this.state, watching: item });
+    watching &&
+      watching.etag !== item.etag &&
+      this.setState({ ...this.state, watching: item });
   };
   render() {
     const watching = this.state.watching;
